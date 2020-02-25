a9a81110aafbf87aebdd57daefb85278e9243024
Gene Lee <gwlee244@gmail.com>
2020-02-25 15:47:07 -0500

    added questions to index.js file

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "githubName",
+            message: "What is your GitHub user name?"
+        },
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "What is your project title?"
+        },
+        {
+            type: "input",
+            name: "projectDescription",
+            message: "Provide a project description."
+        },
+        {
+            type: "input",
+            name: "projectInstallation",
+            message: "Provide instructions on how to get the application to run."
+        },
+    ])
+    .then(answers => {
+        console.info("Answer:", answers);
+    });
